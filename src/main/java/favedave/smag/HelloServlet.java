9ebb54785746cba8cc451259c2d50abe6135ed06
scenaristeur <scenaristeur@gmail.com>
2015-01-04 16:22:24 +0100

    test eclipse

--- src/main/java/favedave/smag/HelloServlet.java
+++ src/main/java/favedave/smag/HelloServlet.java
@@ -20,7 +20,7 @@ public class HelloServlet extends HttpServlet
     {
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>Creation de deux agents</h1>");
+        response.getWriter().println("<h1>Creation de deux agents !</h1>");
         response.getWriter().println("session=" + request.getSession(true).getId());
         
         /* Agents -*/
